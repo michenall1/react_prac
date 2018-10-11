8f77af35829f72bbd83eb06e0425999b440565c1
BowenLiu <bliu3535@gmail.com>
2018-10-11 15:15:14 -0700

    Updates

---
+++ precache-manifest.91a3211d7798077497245b97a926f409.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93648942626a37703c2a",
+    "url": "/react_prac/static/js/runtime~main.93648942.js"
+  },
+  {
+    "revision": "ebb65b1d10fbfb7d40ff",
+    "url": "/react_prac/static/js/main.ebb65b1d.chunk.js"
+  },
+  {
+    "revision": "0e1045f5cba90c169bef",
+    "url": "/react_prac/static/js/1.0e1045f5.chunk.js"
+  },
+  {
+    "revision": "ebb65b1d10fbfb7d40ff",
+    "url": "/react_prac/static/css/main.4a26f5ee.chunk.css"
+  },
+  {
+    "revision": "6ce3e851d93e6e2691ea5d5cd5e6b5d9",
+    "url": "/react_prac/index.html"
+  }
+];
